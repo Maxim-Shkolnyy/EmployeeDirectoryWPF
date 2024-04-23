ab993bb66a5f2e4da7fa33b3563f0915dec7e34f
Maxim-Shkolnyy <105427529+Maxim-Shkolnyy@users.noreply.github.com>
2024-04-23 12:59:04 +0300

    333

--- EmployeeDirectoryWPF/Model/EmployeeModel.cs
+++ EmployeeDirectoryWPF/Model/EmployeeModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace EmployeeDirectoryWPF.Model
 {
-    class EmployeeModel
+    public class EmployeeModel
     {
         public int Id { get; set; }
         public required string Name { get; set; }
---
+++ EmployeeDirectoryWPF/Services/MyDbContext.cs
@@ -0,0 +1,23 @@
+﻿using EmployeeDirectoryWPF.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeDirectoryWPF.Services
+{
+    internal class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base (options)
+        {      
+        }
+        public DbSet<EmployeeModel> Models { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\Local;Database=MaxDB;Integrated Security=True\" providerName=\"System.Data.SqlClient;");
+        }
+    }
+}
