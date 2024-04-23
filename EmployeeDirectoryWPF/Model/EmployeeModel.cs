869f07c115a4184b31ed9dbdd8b465fd1373cb9c
Sh-Maxim-N <sh.maxim.n@gmail.com>
2024-04-23 19:08:03 +0300

    Mi-no

--- EmployeeDirectoryWPF/Model/EmployeeModel.cs
+++ EmployeeDirectoryWPF/Model/EmployeeModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +10,15 @@ namespace EmployeeDirectoryWPF.Model
 {
     public class EmployeeModel
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public required string Name { get; set; }
         public string? Address { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string? Phone { get; set; }
         public string? JobTitle { get; set; }
-        public string? Status { get; set; }
+        public bool Status { get; set; } = true;
         public decimal Salary { get; set; }
         public DateTime DateOfHiring { get; set; } = DateTime.Now;
         public DateTime? DateOfRetirement { get; set; }
