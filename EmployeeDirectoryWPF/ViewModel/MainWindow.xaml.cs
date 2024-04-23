e8cd5ccc016d9429d8ba44fc5cc662afc737a715
Sh-Maxim-N <sh.maxim.n@gmail.com>
2024-04-23 14:28:25 +0300

    Build

--- EmployeeDirectoryWPF/ViewModel/MainWindow.xaml.cs
+++ EmployeeDirectoryWPF/ViewModel/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace EmployeeDirectoryWPF
+namespace EmployeeDirectoryWPF.ViewModel
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
