d9c3db195b7f59eb6cb314a58755ef3cfe8f10b6
Maxim-Shkolnyy <105427529+Maxim-Shkolnyy@users.noreply.github.com>
2024-04-30 00:18:29 +0300

    Edit

--- EmployeeDirectoryWPF/View/MainWindow.xaml.cs
+++ EmployeeDirectoryWPF/View/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using EmployeeDirectoryWPF.Services;
 using EmployeeDirectoryWPF.View;
 using System.Collections.ObjectModel;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace EmployeeDirectoryWPF.ViewModel
 {
@@ -10,6 +11,8 @@ namespace EmployeeDirectoryWPF.ViewModel
     {
         private EmployeeViewModel _viewModel;
         private AddWindow _addUserWindow;
+        private Employee _selectedEmployee;
+
 
 
         public ObservableCollection<Employee> Employees { get; set; }
@@ -25,10 +28,44 @@ namespace EmployeeDirectoryWPF.ViewModel
 
         private void AddButtonClick(object sender, RoutedEventArgs e)
         {
+            this.Opacity = 0.4;
+
             _addUserWindow = new AddWindow();
             _addUserWindow.Owner = this;
-            //this.DialogResult = true;
+            _addUserWindow.Closed += AddWindowClosed;
             _addUserWindow.ShowDialog();
+        }
+
+        private void EditButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (_selectedEmployee != null)
+            {
+                int employeeId = _selectedEmployee.Id;
+                EditEmployee(employeeId);
+            }
+            else
+            {
+            }
+        }
+
+        private void EditEmployee(int employeeId)
+        {
+            // Виконайте редагування співробітника з використанням його ID
+            // Тут ви можете викликати відповідний метод для редагування співробітника у вашому ViewModel
+        }
+
+        private void DataGridSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Отримуємо вибраний об'єкт Employee
+            if (sender is DataGrid dataGrid)
+            {
+                _selectedEmployee = dataGrid.SelectedItem as Employee;
+            }
+        }
+
+        private void AddWindowClosed(object sender, EventArgs e)
+        {
+            this.Opacity = 1;
         }
     }
 }
